17914ce88a0ec2d3eab2bd0cbffef939bc516116
dipo33 <mato.dipcar@gmail.com>
2025-10-28 19:24:12 +0100

    feat: use i18next-icu

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,11 +2,13 @@ import type { I18nToken } from '@/model/data.ts';
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import Backend from 'i18next-http-backend';
+import ICU from 'i18next-icu';
 import { initReactI18next, useTranslation } from 'react-i18next';
 
 void i18n
   .use(Backend)
   .use(LanguageDetector)
+  .use(ICU)
   .use(initReactI18next)
   .init({
     fallbackLng: 'en-US',
